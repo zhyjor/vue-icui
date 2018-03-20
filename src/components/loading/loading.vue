46628231e2d126e6b9fb622fdbd582d7a1e990a8
zhyjor <zhyjor@163.com>
2018-03-20 20:37:07 +0800

    fixed:loading

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cube-loading">
+    <span class="cube-loading-spinners" :style="style">
+      <i class="cube-loading-spinner" v-for="item in balde"/>
+    </span>
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'icui-v-loading'
+
+  export default {
+    name: COMPONENT_NAME,
+    data () {
+      return {
+        balde: 12
+      }
+    },
+    props: {
+      size: {
+        type: Number
+      }
+    },
+    computed: {
+      style () {
+        if (!this.size) {
+          return
+        }
+        const value = `${this.size}px`
+        return {
+          width: value,
+          height: value
+        }
+      }
+    }
+  }
+
+</script>
+
+<style scoped lang="scss">
+  @import '../../common/style/variable.scss';
+
+</style>
