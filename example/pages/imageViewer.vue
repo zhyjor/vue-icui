8278f6ad4330bfbdd956652e604e579c19e58f77
zhyjor <zhyjor@163.com>
2018-04-16 21:35:23 +0800

    add

---
+++ example/pages/imageViewer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <div class="top-close">
+      <div class="close-wrapper">
+        <svg viewBox="0 0 24 24" class=""><g><path d="M13.414 12l5.793-5.793a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 0 0 1.414 1.414L12 13.414l5.793 5.793a.996.996 0 0 0 1.414 0 1 1 0 0 0 0-1.414L13.414 12z"></path></g></svg>
+      </div>
+
+    </div>
+    <div class="container">
+
+    </div>
+    <div bottom-handler></div>
+
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="scss">
+  .wrapper {
+    .top-close {
+      z-index: 2;
+      position: relative;
+      box-direction: normal;
+      box-orient: vertical;
+      flex-direction: column;
+      display: flex;
+      box-sizing: border-box;
+      .close-wrapper{
+        height: 3.5rem;
+        padding:0 0.98rem;
+        box-pack: center;
+        justify-content: center;
+        box-direction: normal;
+        box-orient: horizontal;
+        flex-direction: row;
+        svg{
+          color: rgb(255, 255, 255);
+          height: 1.75rem;
+        }
+      }
+    }
+  }
+
+</style>
---
+++ example/pages/slide.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="wrapper">
+    <div ref="slideWrapper" class="slide-container">
+      <v-icui-slide
+        ref="slide"
+        :data="items"
+        :initial-index="initialIndex"
+        :loop="loop"
+        :auto-play="autoPlay"
+        :interval="interval"
+        :threshold="threshold"
+        :speed="speed"
+        :allow-vertical="allowVertical"
+        @change="changePage"
+        @click="clickPage">
+        <template v-if="dotsSlot" slot="dots" slot-scope="props">
+            <span class="my-dot" :class="{active: props.current === index}"
+                  v-for="(item, index) in props.dots">{{index + 1}}</span>
+        </template>
+      </v-icui-slide>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const item3 = {
+    url: 'http://www.didichuxing.com/',
+    image: '//webapp.didistatic.com/static/webapp/shield/cube-ui-examples-slide03.png'
+  }
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            url: 'http://www.didichuxing.com/',
+            image: require('../assets/demo/1.jpg')
+          }, {
+            url: 'http://www.didichuxing.com/',
+            image: require('../assets/demo/4.jpg')
+          }, {
+            url: 'http://www.didichuxing.com/',
+            image: require('../assets/demo/7.jpg')
+          }, {
+            url: 'http://www.didichuxing.com/',
+            image: require('../assets/demo/8.png')
+          }
+        ],
+        loop: false,
+        autoPlay: false,
+        interval: 4000,
+        threshold: 0.3,
+        speed: 400,
+        allowVertical: false,
+        initialIndex: 1,
+        dotsSlot: false,
+        addItem3: false
+      }
+    },
+    watch: {
+      addItem3 (newV) {
+        if (newV) {
+          this.items.push(item3)
+        } else {
+          this.items.pop()
+        }
+      }
+    },
+    methods: {
+      changePage (current) {
+        console.log('当前轮播图序号为:' + current)
+      },
+      clickPage (item, index) {
+        console.log(item, index)
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .wrapper
+    width: 100%
+    background: #efeff4
+
+  .slide-container {
+    /*height: 75px;*/
+    width 100%
+    margin-bottom: 15px;
+    transform: translateZ(0px);
+    border-radius: 2px;
+    overflow: hidden;
+    box-shadow: 0 2px 9px #ddd;
+    .cube-slide-dots {
+      .my-dot {
+        height: auto;
+        font-size: 12px;
+        background: none;
+        &.active {
+          color: #fc9153;
+        }
+      }
+    }
+  }
+
+
+</style>
